3661a678f28f03017d2a92321c0ee2e5bedc35de
frank.lilla <frank.lilla@stud.uni-obuda.hu>
2023-04-22 18:49:18 +0200

    update gyak08(fit)

--- GYAK/GYAK08/LinearRegressionSkeleton.py
+++ GYAK/GYAK08/LinearRegressionSkeleton.py
@@ -5,15 +5,12 @@ from sklearn.model_selection import train_test_split
 class LinearRegression:
        
     def __init__(self, epochs: int = 1000, lr: float = 1e-3): 
-        self.X_train = np.array
-        self.y_train= np.array
-        self.X_test = np.array
-        self.y_test = np.array
+   
         self.epochs = epochs
         self.lr = lr
     
 
-    def fit(self, X: np.array, y: np.array):
+    def fit(self, X: np.array, y: np.array): # x: training data, y: target value
       #  self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         self.m = 0 #meredekség
         self.c = 0 #metszés x tengelyel
@@ -22,17 +19,17 @@ class LinearRegression:
  
         losses = []
         for i in range(self.epochs): 
-            self.y_pred = self.m*self.X_train + self.c  # The current predicted value of Y
+            self.y_pred = self.m*X + self.c  # The current predicted value of Y
 
-            residuals = self.y_pred - self.y_train
+            residuals = self.y_pred - y
             loss = np.sum(residuals ** 2)
             losses.append(loss)
-            D_m = (-2/n) * sum(self.X_train * residuals)  # Derivative wrt m
+            D_m = (-2/n) * sum(X * residuals)  # Derivative wrt m
             D_c = (-2/n) * sum(residuals)  # Derivative wrt c
             self.m = self.m + self.lr * D_m  # Update m
             self.c = self.c + self.lr * D_c  # Update c
             if i % 100 == 0:
-                (np.mean(self.y_train-self.y_pred))
+                (np.mean(y-self.y_pred))
 
 
         
@@ -43,8 +40,8 @@ class LinearRegression:
 
     def predict(self, X):
         pred = []
-        for X in self.X_test:
-            self.y_pred = self.m*X + self.c
+        for Xi in X:
+            self.y_pred = self.m*Xi + self.c
             pred.append(self.y_pred)
 
         return pred
