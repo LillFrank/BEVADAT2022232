4af869ab14cb86af295c330a7b218d85e83b42ef
frank.lilla <frank.lilla@stud.uni-obuda.hu>
2023-04-29 20:18:59 +0200

    update hazi09

--- HAZI/HAZI09/HAZI09.py
+++ HAZI/HAZI09/HAZI09.py
@@ -8,28 +8,35 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import accuracy_score
 from scipy.stats import mode
 from sklearn.metrics import confusion_matrix
-import sklearn.datasets as datasets
+from sklearn.datasets import load_digits
 
 
 class KMeansOnDigits():
 
     def __init__(self, n_clusters, random_state) -> None:
-        self.nn_clusters = n_clusters
+        self.n_clusters = n_clusters
         self.random_state = random_state
 
     def load_digits(self):
-        self.digits =  datasets.load_digits()
+        self.digits = load_digits()
     
     def predict(self)-> KMeans and  np.ndarray:
         kmean = KMeans(n_clusters=self.n_clusters,random_state=self.random_state)
         self.clusters = kmean.fit_predict(self.digits.data, self.digits.target)
 
     def get_labels(self):
-        pass
+        result = np.empty(self.clusters.shape)
+
+        for i in self.digits.target_names:
+            ms = i==self.clusters
+            subarray = self.digits.target[ms]
+            mod = mode(subarray).mode.item()
+            result[ms] = mod
+        self.labels = result
 
     def calc_accuracy(self):
-        pass
+        self.accuracy = accuracy_score(self.digits.target,self.labels)
 
     def confusion_matrix(self):
-        pass
+        self.mat = confusion_matrix(self.digits.target, self.labels)
        
